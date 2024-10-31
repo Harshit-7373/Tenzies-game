bea5ae6490f5f07e83c7cf76accf84352cb5bb30
Harshit <harshit.sansanwal2528@gmail.com>
2024-10-31 11:42:37 +0530

    Updates

---
+++ precache-manifest.ecaf375347de8c27d2f7407e81ad2462.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b58bb1d1779880ae2361",
+    "url": "/tenzies-game/static/css/main.0cccccce.chunk.css"
+  },
+  {
+    "revision": "b58bb1d1779880ae2361",
+    "url": "/tenzies-game/static/js/main.b58bb1d1.chunk.js"
+  },
+  {
+    "revision": "3e1bd8dbe8de0fe8a5d3",
+    "url": "/tenzies-game/static/js/1.3e1bd8db.chunk.js"
+  },
+  {
+    "revision": "1d2148d14866088b7247",
+    "url": "/tenzies-game/static/js/runtime~main.1d2148d1.js"
+  },
+  {
+    "revision": "cb7831f0a58a2f903c5be1d8810b6765",
+    "url": "/tenzies-game/index.html"
+  }
+];
